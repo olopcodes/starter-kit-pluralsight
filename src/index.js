6e4ade564b9ea49e89aa750f823280fd32bd9500
olopcodes <olopcodes@gmail.com>
2021-05-26 21:56:39 -0400

    added eslint

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import "./index.css";
 import numeral from "numeral";
 
 const courseValue = numeral(1000).format("$0,0.00");
-debugger;
+// debugger;
 console.log(`i would pay ${courseValue} for this course`);
